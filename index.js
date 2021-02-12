a5f7ab417a34ffcb533d912a338f08abf67eee56
Javier Cardenas <glatorian@gmail.com>
2021-02-12 18:52:40 -0500

    set up file structure

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 const jest = require("jest");
+//require constructors in lib library
+const Employee = ("./lib/Employee");
+const Engineer = ("./lib/Engineer");
+const Intern = ("./lib/Intern");
+const Manager = ("./lib/Manager");
+
+const employees = [];
+
+initalizeApp => {
+    startHtml();
+    addMember();
+}
