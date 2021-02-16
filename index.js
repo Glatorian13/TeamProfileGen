731f7d7c7b287d0ab6f1dc7545d638ab14747391
Javier Cardenas <glatorian@gmail.com>
2021-02-15 19:18:07 -0500

    tweaks

--- index.js
+++ index.js
@@ -3,46 +3,62 @@ import fs from "fs";
 import inquirer from "inquirer";
 import jest from "jest";
 //require constructors in lib library
-const Employee = ("./lib/Employee");
-const Manager = ("./lib/Manager");
-const Engineer = ("./lib/Engineer");
-const Intern = ("./lib/Intern");
+const Employee = "./lib/Employee";
+const Manager = "./lib/Manager";
+const Engineer = "./lib/Engineer";
+const Intern = "./lib/Intern";
 
 //run inquirer to gather input data
 function userInput() {
-    inquirer
-        .prompt([{
-            type: "input",
-            message: "Enter employee name.",
-            name: "name",
-        }, {
-            type: "number",
-            message: "Enter employee's ID number.",
-            name: "id",
-        }, {
-            type: "input",
-            message: "Enter employee's email address.",
-            name: "email",
-        }, {
-            type: "list",
-            message: "Enter employee's role",
-            name: "role",
-            choices: ["Engineer", "Intern", "Manager"]
-        }])
-        .then(
-            function ({ name, id, email, role }) {
-                switch (role) {
-                    case "Engineer":
-                        inquirer
-                            .prompt({
-                                type: "input",
-                                message: "Enter GitHub user-name.",
-                                name: "github"
-                            }).then(
-                                function ({ github }) {
-                                    generateEngineer(name, id, email, github)
-                                })
-                })
-}
-        )
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Enter employee name.",
+        name: "name",
+      },
+      {
+        type: "number",
+        message: "Enter employee's ID number.",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "Enter employee's email address.",
+        name: "email",
+      },
+      {
+        type: "list",
+        message: "Enter employee's role",
+        name: "role",
+        choices: ["Engineer", "Intern", "Manager"],
+      },
+    ])
+    .then(function ({ name, id, email, role }) {
+      switch (role) {
+        case "Engineer":
+          inquirer
+            .prompt({
+              type: "input",
+              message: "Enter GitHub user-name.",
+              name: "github",
+            })
+            .then(function ({ github }) {
+              generateEngineer(name, id, email, github);
+              addMember();
+            }) 
+            break 
+            case "Intern":
+                inquirer
+                .prompt({
+                    type: "input",
+                    message: "Enter school attended.",
+                    name: "school"
+                }).then(
+                    function ({ school }) {
+                        generateIntern
+                    }
+                )
+      }
+    })
 }
